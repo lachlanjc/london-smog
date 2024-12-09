89cb7d9cbc4c062b68bc353c34daeb7b4e76f019
Lachlan Campbell <lachlanjc@hey.com>
2024-12-09 18:40:52 -0500

    Flesh out stations map

--- site/src/Stations/Stations.jsx
+++ site/src/Stations/Stations.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect } from "react";
 import stations from "./stations.json";
-
+import NumberFlow from "@number-flow/react";
 import Map, { Marker } from "react-map-gl";
 
 const MAPBOX_TOKEN =
@@ -9,11 +9,11 @@ const MAPBOX_TOKEN =
 // type Markers = Array<MarkerProps & { key: string }>;
 
 const STATUS_COLORS = {
-  Coal: "rose",
-  "Coal, Diesel": "red",
-  Diesel: "amber",
-  "Oil, Gas": "neutral",
-  Waste: "sky",
+  Gas: "accent2",
+  Coal: "accent",
+  Diesel: "amber-500",
+  Oil: "neutral-500",
+  Waste: "avocado-600",
 }; // as const;
 const STATUS_KEYS = Object.keys(STATUS_COLORS);
 // as Array<
@@ -23,17 +23,32 @@ const STATUS_KEYS = Object.keys(STATUS_COLORS);
 const initialViewState = {
   latitude: 51.5074,
   longitude: -0.1278,
-  zoom: 10.5,
+  zoom: 9,
   bearing: 0,
   pitch: 20,
 };
 
+const emptyStation = {
+  name: "---",
+  lat: "51.4694444",
+  lng: "-0.3506305",
+  type: "Coal",
+  mw: "1",
+  opened: "1900",
+  closed: "2000",
+  demolished: "Yes",
+};
+
 export default function Stations() {
   const mapRef = useRef(null);
-  const rootRef = useRef(null);
+  const [activeStation, setActiveStation] = useState(emptyStation);
 
   return (
     <>
+      <div
+        className="bg-amber-500 bg-avocado-600 bg-lime-500 bg-accent bg-accent2 bg-neutral-500"
+        hidden
+      />
       <style>{`.mapboxgl-canvas, .mapboxgl-marker { position: absolute !important; }`}</style>
       <Map
         ref={mapRef}
@@ -43,6 +58,8 @@ export default function Stations() {
         style={{
           width: "100%",
           height: "100vh",
+          overflow: "hidden",
+          backgroundColor: "#222",
         }}
         cooperativeGestures
         scrollZoom={false}
@@ -59,39 +76,113 @@ export default function Stations() {
             <div
               className={`h-6 w-6 rounded-full opacity-75 bg-${
                 STATUS_COLORS[marker["type"]]
-              }-500`}
-              title={marker.key}
+              }`}
+              style={{
+                transform: `scale(${typeof Number(marker.mw) === "number" ? Number(marker.mw) / 100 : 1})`,
+              }}
+              title={marker.name}
+              onMouseEnter={() => setActiveStation(marker)}
             />
           </Marker>
         ))}
 
-        <div
-          className="bg-amber-500 bg-red-500 bg-sky-500 bg-rose-500 bg-neutral-500"
-          hidden
-        />
         {/* <div className="z-1 absolute bottom-0 left-0 right-0 h-[40vh] bg-gradient-to-b from-transparent to-black/75" /> */}
         <div
-          className="z-2 absolute bottom-24 left-2/4 -translate-x-1/2 text-center tracking-snug text-white [text-wrap:balance] md:max-w-3xl"
+          className="z-2 absolute top-12 left-12 tracking-snug text-white [text-wrap:balance] md:max-w-3xl"
           style={{ textShadow: "0 2px 4px rgba(0,0,0,0.75)" }}
         >
           <p className="text-lg md:text-4xl">
-            Across the twentieth century, over 20 power stations polluted
-            London.
+            Across the twentieth century, over{" "}
+            <strong className="font-bold">
+              thirty coal power plants polluted
+            </strong>{" "}
+            London—not including the factories and every house burning more
+            coal.
           </p>
         </div>
         <div
-          className="absolute bottom-12 right-12 flex flex-col gap-3 text-white"
+          className="absolute top-12 right-12 flex flex-col gap-2 text-white bg-black/25 transition-opacity [transition-duration:0.25s] rounded-lg p-6 w-md"
+          style={{ opacity: activeStation.name === emptyStation.name ? 0 : 1 }}
+        >
+          <header className="flex gap-2 items-start justify-start">
+            <strong className="font-bold text-2xl text-balance flex-auto">
+              {activeStation.name} Station
+            </strong>
+            <button
+              className="w-12 h-12 inline-block text-center text-neutral-300 hover:text-neutral-50 transition-colors text-3xl relative -top-3 -right-3"
+              onClick={() => setActiveStation(emptyStation)}
+            >
+              ×
+            </button>
+          </header>
+          <dl className="grid grid-cols-6 gap-3">
+            <div className="col-span-2">
+              <dt className="text-neutral-500">Power source</dt>
+              <dd className={`flex items-center gap-2 capitalize`}>
+                <div
+                  className={`h-4 w-4 rounded-full bg-${
+                    STATUS_COLORS[activeStation.type]
+                  }`}
+                />
+                {activeStation.type.replace("-", " ")}
+              </dd>
+            </div>
+            <div className="col-span-4">
+              <dt className="text-neutral-500">Capacity</dt>
+              <dd>
+                <NumberFlow
+                  value={activeStation.mw}
+                  format={{ useGrouping: false }}
+                />
+                {" MW"}
+              </dd>
+            </div>
+            <div className="col-span-2">
+              <dt className="text-neutral-500">Opened</dt>
+              <dd>
+                {activeStation.opened ? (
+                  <NumberFlow
+                    value={activeStation.opened}
+                    format={{ useGrouping: false }}
+                  />
+                ) : (
+                  "—"
+                )}
+              </dd>
+            </div>
+            <div className="col-span-2">
+              <dt className="text-neutral-500">Closed</dt>
+              <dd>
+                {activeStation.closed ? (
+                  <NumberFlow
+                    value={activeStation.closed}
+                    format={{ useGrouping: false }}
+                  />
+                ) : (
+                  "—"
+                )}
+              </dd>
+            </div>
+            <div className="col-span-2">
+              <dt className="text-neutral-500">Demolished</dt>
+              <dd>{activeStation.demolished}</dd>
+            </div>
+          </dl>
+        </div>
+
+        <div
+          className="absolute bottom-12 left-12 flex flex-col gap-3 text-white"
           aria-hidden
         >
           <dl>
             {Object.keys(STATUS_COLORS).map((status, i) => (
-              <div className={`flex items-center justify-end gap-2`}>
-                <dd className="capitalize">{status.replace("-", " ")}</dd>
+              <div className={`flex items-center gap-2`}>
                 <div
                   className={`h-4 w-4 rounded-full bg-${
                     STATUS_COLORS[status] // as keyof typeof STATUS_COLORS]
-                  }-500`}
+                  }`}
                 />
+                <dd className="capitalize">{status.replace("-", " ")}</dd>
               </div>
             ))}
           </dl>
