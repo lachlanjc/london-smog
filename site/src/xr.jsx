0a3f0d85fd3f55b0b2baf47e5c11a76147ff8880
Lachlan Campbell <lachlanjc@hey.com>
2024-12-10 17:51:29 -0500

    Begin XR embed

---
+++ site/src/xr.jsx
@@ -0,0 +1,43 @@
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { Canvas, extend, useFrame, useLoader } from "@react-three/fiber";
+import { XR, createXRStore } from "@react-three/xr";
+
+import {
+  Html,
+  //   Environment,
+  //   Image,
+  //   // ScrollControls,
+  //   Billboard,
+  //   // Text,
+} from "@react-three/drei";
+import App from "./App.jsx";
+import "./index.css";
+
+const store = createXRStore();
+
+function XRApp() {
+  return (
+    <>
+      <button onClick={() => store.enterAR()}>Enter AR</button>
+      <Canvas>
+        <XR store={store}>
+          <pointLight position={[10, 10, 10]} intensity={1.5} />
+          <Suspense fallback={null}>
+            <group rotation={[0, Math.PI, 0]} position={[0, 0, 0]}>
+              <Html className="browser" position={[0, 0, 0]} center>
+                <App />
+              </Html>
+            </group>
+          </Suspense>
+        </XR>
+      </Canvas>
+    </>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <XRApp />
+  </React.StrictMode>,
+);
